7d309aaad735e9e221ca9da1d24baa385a82ab5d
Claire Froelich <claire.froelich@gmail.com>
2020-11-29 19:42:41 -0800

    Add dynamic default Lang to util

--- src/utils/getLangFromUrl.ts
+++ src/utils/getLangFromUrl.ts
@@ -1,8 +1,9 @@
 import { useLocation } from "@reach/router"
+import { config } from "../../config"
 
 // TODO: set "/" to redirect to "/en"
 export const getLangFromUrl = (): Languages => {
   const { pathname } = useLocation()
   const match = pathname.match(/^\/(\w+)\//)
-  return match ? (match[1] as Languages) : "en"
+  return match ? (match[1] as Languages) : (config.defaultLang as Languages)
 }
